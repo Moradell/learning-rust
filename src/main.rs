e57f530bfe98498ab2a981eaaef8a10191094adb
Aleksey <themoradell@gmail.com>
2022-05-31 18:59:33 +0300

    Mut and unmut var

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello world from console");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
