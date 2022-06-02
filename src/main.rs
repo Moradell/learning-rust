8b7a9fb7076b56c95f6437473e481a558cd8ec49
Aleksey <themoradell@gmail.com>
2022-06-02 21:04:36 +0300

    Math operations

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
-    let x = 5;
-    
-    let x = x + 1;
+    // addition
+    let sum = 5 + 10;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    // subtraction
+    let difference = 95.5 - 4.3;
 
-    println!("The value of x is: {x}");
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+
+    // remainder
+    let remainder = 43 % 5;
 }
