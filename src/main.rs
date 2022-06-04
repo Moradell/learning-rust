8aadb0d5f68e2c0feab2ffb6e18e67c8370dcf13
Aleksey <themoradell@gmail.com>
2022-06-04 21:07:15 +0300

    Check function work

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,11 @@
 fn main() {
-    // addition
-    let sum = 5 + 10;
+    print_labeled_measurement(five(), 'h');
+}
 
-    // subtraction
-    let difference = 95.5 - 4.3;
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
 
-    // multiplication
-    let product = 4 * 30;
-
-    // division
-    let quotient = 56.7 / 32.2;
-    let floored = 2 / 3; // Results in 0
-
-    // remainder
-    let remainder = 43 % 5;
-
-    let t = true;
-
-    let f: bool = false;
-    
-    let c: char = 'z';
-
-    let heart_eyed_cat = '😻';
-
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-
-    let five_hundred = tup.1;
-
-    let (x, y, z) = tup;
-
-    println!("The value of y is: {y}");
-
-    let months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
-
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-
-    let a = [3; 5];
+fn five() -> i32 {
+    5
 }
